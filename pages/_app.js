0f81f8bfbcf3b2b42856ddae5bd28c44b2f63374
BayezidMostafa <hiperx1@gmail.com>
2023-12-20 03:22:46 +0600

    profile updating

--- pages/_app.js
+++ pages/_app.js
@@ -11,26 +11,31 @@ import { GlobalProvider } from 'Context/Context'
 import { SessionProvider } from 'next-auth/react'
 import Head from 'next/head'
 import AuthProvider from 'Context/AuthProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
     <AnimatePresence mode="wait">
-      <SessionProvider>
-        <GlobalProvider>
-          <AuthProvider>
-            <ManagedUIContext>
-              <Head>
-                <title>Hola Home</title>
-              </Head>
-              <motion.div className="max-w-screen overflow-hidden">
-                <Layout>
-                  <Component {...pageProps} />
-                </Layout>
-              </motion.div>
-            </ManagedUIContext>
-          </AuthProvider>
-        </GlobalProvider>
-      </SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider>
+          <GlobalProvider>
+            <AuthProvider>
+              <ManagedUIContext>
+                <Head>
+                  <title>Hola Home</title>
+                </Head>
+                <motion.div className="max-w-screen overflow-hidden">
+                  <Layout>
+                    <Component {...pageProps} />
+                  </Layout>
+                </motion.div>
+              </ManagedUIContext>
+            </AuthProvider>
+          </GlobalProvider>
+        </SessionProvider>
+      </QueryClientProvider>
     </AnimatePresence>
   )
 }
